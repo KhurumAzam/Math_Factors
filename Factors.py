5e5e60e3a5e02b7fe4f19273e4cf927440433eb0
KhurumAzam <76566157+KhurumAzam@users.noreply.github.com>
2023-03-10 07:48:34 -0500

    Update Factors.py

--- Factors.py
+++ Factors.py
@@ -13,17 +13,28 @@ class Factors:
         except Exception as e:
             print("Error in calculation:",e)
 
+    def list_factors(self):
+        temp = tuple(set(self.nums))
+        temp2 = []
+        for i in tuple(set(self.nums)):
+            for j in i:
+                temp2.append(j)
+        temp2.sort()
+        print(f"Factors are: {temp2}")
+
 if __name__ == "__main__":
     obj = Factors()
     while True:
         try:
             num = int(input("What's the number you want factor:"))
             obj.factors(num)
+            obj.list_factors()
             try:
                 choice = input("Do you want to exit program (y or n):")
                 if choice == "y":
                     break
                 else:
+                    obj.nums = []
                     continue
             except Exception as e:
                 print("Error in input, please enter a valid input")
